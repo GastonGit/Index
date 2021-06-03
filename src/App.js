502c3436b7d211ff7c0865db0bc8cd2c9e21bfad
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-06-03 18:04:56 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <header className="Head">
+            <p>
+                GastonGit
+            </p>
+        </header>
+      <div className="Projects">
+          <div className="Project">
+              <p className="Project-name">Hot Twitch Clips</p>
+              <p className="Project-description">Automatically clips exciting and funny moments from Twitch in real-time</p>
+              <a className="Project-link" href="/hotclips">Live Example</a>
+              <a className="Project-link" href="https://github.com/GastonGit/Hot-Twitch-Clips">Github</a>
+          </div>
+          <div className="Project">
+              <p className="Project-name">HangulTrainer</p>
+              <p className="Project-description">A simple game for learning the korean alphabet <i>hangul</i>.</p>
+              {/*<a className="Project-link" href="/">Live Example</a>*/}
+              <a className="Project-link" href="https://github.com/GastonGit/HangulTrainer">Github</a>
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
