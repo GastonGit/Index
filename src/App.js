d3429fe8ad660a58b9879c2b02677f676f05dd10
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-06-12 18:21:58 +0200

    add simple fade-in for projects

--- src/App.js
+++ src/App.js
@@ -1,30 +1,52 @@
 import React from "react";
 import './App.css';
+import { Fade } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-        <header className="Head">
-            <a href="https://github.com/GastonGit" className="Header-link" >
-                GastonGit
-            </a>
-        </header>
-      <div className="Projects">
-          <div className="Project">
-              <p className="Project-name">Hot Twitch Clips</p>
-              <p className="Project-description">Automatically clips exciting and funny moments from Twitch in real-time</p>
-              <a className="Project-link" href="/hotclips">Live Example</a>
-              <a className="Project-link" href="https://github.com/GastonGit/Hot-Twitch-Clips">Github</a>
-          </div>
-          <div className="Project">
-              <p className="Project-name">HangulTrainer</p>
-              <p className="Project-description">A simple game for learning the korean alphabet <i>hangul</i>.</p>
-              {/*<a className="Project-link" href="/">Live Example</a>*/}
-              <a className="Project-link" href="https://github.com/GastonGit/HangulTrainer">Github</a>
-          </div>
-      </div>
-    </div>
-  );
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {fade1: false, fade2: false};
+    }
+
+    componentDidMount() {
+
+        setTimeout(function (){
+            this.setState({fade1: true})
+        }.bind(this), 200)
+
+        setTimeout(function (){
+            this.setState({fade2: true})
+        }.bind(this), 600)
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="Head">
+                    <a href="https://github.com/GastonGit" className="Header-link" >
+                        GastonGit
+                    </a>
+                </header>
+                <div className="Projects">
+                    <Fade in={this.state.fade1}>
+                        <div className="Project">
+                            <p className="Project-name">Hot Twitch Clips</p>
+                            <p className="Project-description">Automatically clips exciting and funny moments from Twitch in real-time</p>
+                            <a className="Project-link" href="/hotclips">Live Example</a>
+                            <a className="Project-link" href="https://github.com/GastonGit/Hot-Twitch-Clips">Github</a>
+                        </div>
+                    </Fade>
+                    <Fade in={this.state.fade2}>
+                        <div className="Project">
+                            <p className="Project-name">HangulTrainer</p>
+                            <p className="Project-description">A simple game for learning the korean alphabet <i>hangul</i>.</p>
+                            {/*<a className="Project-link" href="/">Live Example</a>*/}
+                            <a className="Project-link" href="https://github.com/GastonGit/HangulTrainer">Github</a>
+                        </div>
+                    </Fade>
+                </div>
+            </div>
+        );
+    }
 }
-
-export default App;
