370c3c4f5d8ba16c82ad8501310c4bf254f4b0c3
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-09-03 14:02:09 +0200

    refactor content data

--- src/App.js
+++ src/App.js
@@ -21,6 +21,23 @@ export default class App extends React.Component {
     }
 
     render() {
+        const data = [
+            {
+                "name": "Hot Twitch Clips",
+                "desc": "Automatically clips exciting and funny moments from Twitch in real-time",
+                "progress": "In the final stages of development",
+                "liveUrl": "/hotclips",
+                "github": "https://github.com/GastonGit/Hot-Twitch-Clips"
+            },
+            {
+                "name": "HangulTrainer",
+                "desc": "A simple game for learning the korean alphabet hangul.",
+                "progress": "In development",
+                "liveUrl": null,
+                "github": "https://github.com/GastonGit/HangulTrainer"
+            },
+        ]
+
         return (
             <div className="App">
                 <header className="Head">
@@ -31,20 +48,20 @@ export default class App extends React.Component {
                 <div className="Projects">
                     <Fade in={this.state.fade1}>
                         <div className="Project">
-                            <p className="Project-name">Hot Twitch Clips</p>
-                            <p className="Project-description">Automatically clips exciting and funny moments from Twitch in real-time</p>
-                            <i className="Project-progress">In the final stages of development</i>
-                            <a className="Project-link" href="/hotclips">Live Example</a>
-                            <a className="Project-link" href="https://github.com/GastonGit/Hot-Twitch-Clips">Github</a>
+                            <p className="Project-name">{data[0].name}</p>
+                            <p className="Project-description">{data[0].desc}</p>
+                            <i className="Project-progress">{data[0].progress}</i>
+                            {data[0].liveUrl !== null && <a className="Project-link" href={data[0].liveUrl}>Live Example</a>}
+                            <a className="Project-link" href={data[0].github}>Github</a>
                         </div>
                     </Fade>
                     <Fade in={this.state.fade2}>
                         <div className="Project">
-                            <p className="Project-name">HangulTrainer</p>
-                            <p className="Project-description">A simple game for learning the korean alphabet <i>hangul</i>.</p>
-                            <i className="Project-progress">In development</i>
-                            {/*<a className="Project-link" href="/">Live Example</a>*/}
-                            <a className="Project-link" href="https://github.com/GastonGit/HangulTrainer">Github</a>
+                            <p className="Project-name">{data[1].name}</p>
+                            <p className="Project-description">{data[1].desc}</p>
+                            <i className="Project-progress">{data[1].progress}</i>
+                            {data[1].liveUrl !== null && <a className="Project-link" href={data[1].liveUrl}>Live Example</a>}
+                            <a className="Project-link" href={data[1].github}>Github</a>
                         </div>
                     </Fade>
                 </div>
