663c662100dc8e45c8e33b75f6720b1a6a741d8d
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-09-04 16:25:46 +0200

    refactor previous js files into tsx and replace boiler css/html files with previous files

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,35 +1,103 @@
 import React from 'react';
-import styles from '../styles/sApp';
-import { withStyles } from '@material-ui/core';
-import Boil from '../lib/Boil';
+import '../styles/App.css';
+import { Fade } from '@material-ui/core';
 
-const boil = new Boil('rice');
-
-interface AppProps {
-    classes: {
-        App: string;
-        'App-header': string;
-    };
+interface AppState {
+    fade1: boolean;
+    fade2: boolean;
 }
 
-class App extends React.Component<AppProps, unknown> {
-    constructor(props: AppProps) {
+export default class App extends React.Component<unknown, AppState> {
+    constructor(props: unknown) {
         super(props);
-        boil.boilFood();
+        this.state = { fade1: false, fade2: false };
+    }
+
+    componentDidMount(): void {
+        setTimeout(() => {
+            this.setState({ fade1: true });
+        }, 200);
+
+        setTimeout(() => {
+            this.setState({ fade2: true });
+        }, 600);
     }
 
     render(): JSX.Element {
-        const { classes } = this.props;
-        const food = boil.getBoiledFood();
+        const data = [
+            {
+                name: 'Hot Twitch Clips',
+                desc: 'Automatically clips exciting and funny moments from Twitch in real-time',
+                progress: 'In the final stages of development',
+                liveUrl: '/hotclips',
+                github: 'https://github.com/GastonGit/Hot-Twitch-Clips',
+            },
+            {
+                name: 'HangulTrainer',
+                desc: 'A simple game for learning the korean alphabet hangul.',
+                progress: 'In development',
+                liveUrl: null,
+                github: 'https://github.com/GastonGit/HangulTrainer',
+            },
+        ];
 
         return (
-            <div className={classes.App}>
-                <header className={classes['App-header']}>
-                    <p>{food}</p>
+            <div className="App">
+                <header className="Head">
+                    <a
+                        href="https://github.com/GastonGit"
+                        className="Header-link"
+                    >
+                        GastonGit
+                    </a>
                 </header>
+                <div className="Projects">
+                    <Fade in={this.state.fade1}>
+                        <div className="Project">
+                            <p className="Project-name">{data[0].name}</p>
+                            <p className="Project-description">
+                                {data[0].desc}
+                            </p>
+                            <i className="Project-progress">
+                                {data[0].progress}
+                            </i>
+                            {data[0].liveUrl !== null && (
+                                <a
+                                    className="Project-link"
+                                    href={data[0].liveUrl}
+                                >
+                                    Live Example
+                                </a>
+                            )}
+                            <a className="Project-link" href={data[0].github}>
+                                Github
+                            </a>
+                        </div>
+                    </Fade>
+                    <Fade in={this.state.fade2}>
+                        <div className="Project">
+                            <p className="Project-name">{data[1].name}</p>
+                            <p className="Project-description">
+                                {data[1].desc}
+                            </p>
+                            <i className="Project-progress">
+                                {data[1].progress}
+                            </i>
+                            {data[1].liveUrl !== null && (
+                                <a
+                                    className="Project-link"
+                                    href={data[1].liveUrl}
+                                >
+                                    Live Example
+                                </a>
+                            )}
+                            <a className="Project-link" href={data[1].github}>
+                                Github
+                            </a>
+                        </div>
+                    </Fade>
+                </div>
             </div>
         );
     }
 }
-
-export default withStyles(styles)(App);
