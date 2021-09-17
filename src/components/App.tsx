5f6e2034ae8639aa5d0f65d5396b905dde972905
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-09-17 13:44:47 +0200

    refactor, move project logic to its own component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,48 +1,14 @@
 import React from 'react';
 import '../styles/App.css';
-import FadeIn from 'react-fade-in';
-import projects from '../projectData.json';
+import Projects from './Projects';
 
-interface AppState {
-    projects: Array<JSX.Element>;
-}
-
-export default class App extends React.Component<unknown, AppState> {
+export default class App extends React.Component<unknown, unknown> {
     constructor(props: unknown) {
         super(props);
-        this.state = { projects: [] };
-        this.getProjects = this.getProjects.bind(this);
-    }
-
-    private fadeInDelay = 300;
-
-    componentDidMount(): void {
-        this.getProjects();
-    }
-
-    getProjects(): void {
-        this.setState({
-            projects: projects.map((project, index) => (
-                <div className="Project" key={'project-' + index}>
-                    <p className="Project-name">{project.name}</p>
-                    <p className="Project-description">{project.desc}</p>
-                    <i className="Project-progress">{project.progress}</i>
-                    {project.liveUrl !== null && (
-                        <a className="Project-link" href={project.liveUrl}>
-                            Releases
-                        </a>
-                    )}
-                    <a className="Project-link" href={project.github}>
-                        Github
-                    </a>
-                </div>
-            )),
-        });
+        this.state = {};
     }
 
     render(): JSX.Element {
-        const { projects } = this.state;
-
         return (
             <div className="App">
                 <header className="Head">
@@ -53,9 +19,7 @@ export default class App extends React.Component<unknown, AppState> {
                         GastonGit
                     </a>
                 </header>
-                <div className="Projects">
-                    <FadeIn delay={this.fadeInDelay}>{projects}</FadeIn>
-                </div>
+                <Projects />
             </div>
         );
     }
---
+++ src/components/Projects.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import '../styles/App.css';
+import FadeIn from 'react-fade-in';
+import projects from '../projectData.json';
+
+interface AppState {
+    projects: Array<JSX.Element>;
+}
+
+export default class Projects extends React.Component<unknown, AppState> {
+    constructor(props: unknown) {
+        super(props);
+        this.state = { projects: [] };
+        this.getProjects = this.getProjects.bind(this);
+    }
+
+    private fadeInDelay = 300;
+
+    componentDidMount(): void {
+        this.getProjects();
+    }
+
+    getProjects(): void {
+        this.setState({
+            projects: projects.map((project, index) => (
+                <div className="Project" key={'project-' + index}>
+                    <p className="Project-name">{project.name}</p>
+                    <p className="Project-description">{project.desc}</p>
+                    <i className="Project-progress">{project.progress}</i>
+                    {project.liveUrl !== null && (
+                        <a className="Project-link" href={project.liveUrl}>
+                            Releases
+                        </a>
+                    )}
+                    <a className="Project-link" href={project.github}>
+                        Github
+                    </a>
+                </div>
+            )),
+        });
+    }
+
+    render(): JSX.Element {
+        const { projects } = this.state;
+
+        return (
+            <div className="Projects">
+                <FadeIn delay={this.fadeInDelay}>{projects}</FadeIn>
+            </div>
+        );
+    }
+}
