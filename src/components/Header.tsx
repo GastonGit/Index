80a4798ef93550f0e5c4681cba34bc08a82e0206
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-09-22 15:19:53 +0200

    add a github logo that redirects to github/gastongit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import '../styles/Header.css';
+import githubLogo from '../assets/GitHub-Mark-Light-120px-plus.png';
 
 export default class Header extends React.Component<unknown, unknown> {
     render(): JSX.Element {
         return (
             <header className="Head">
                 <div className="HeaderContent">
-                    <a
-                        href="https://github.com/GastonGit"
-                        className="Header-link"
-                    >
-                        GastonGit
-                    </a>
+                    <div className="HeaderContentMain">
+                        <a
+                            href="https://github.com/GastonGit"
+                            className="Header-link"
+                        >
+                            GastonGit
+                        </a>
+                        <a
+                            href="https://github.com/GastonGit/"
+                            className="HeaderItem"
+                        >
+                            <img src={githubLogo} alt="Github logo" />
+                        </a>
+                    </div>
                 </div>
             </header>
         );
