bb37dab92525fc17552e7e805a814ada05b098e0
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-09-26 13:56:54 +0200

    create description component and accompanying stylesheet

---
+++ src/components/Description.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/Description.css.css';
+import FadeIn from 'react-fade-in';
+
+export default class Description extends React.Component<unknown, unknown> {
+    constructor(props: unknown) {
+        super(props);
+        this.state = {};
+    }
+
+    private fadeInDelay = 100;
+
+    render(): JSX.Element {
+        return (
+            <FadeIn className="" delay={this.fadeInDelay}>
+                <p>Welcome to GastonGit!</p>
+            </FadeIn>
+        );
+    }
+}
