762c64b8d9faa6c8cd00fcf55e1982ee7ce95c1e
GastonGit <73943682+GastonGit@users.noreply.github.com>
2022-01-25 15:36:39 +0100

    rework header to make use of mui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,122 @@
 import React from 'react';
-import '../styles/Header.css';
-import githubLogo from '../assets/GitHub-Mark-Light-120px-plus.png';
+import {
+    AppBar,
+    Box,
+    Button,
+    Container,
+    IconButton,
+    Link,
+    Menu,
+    MenuItem,
+    Toolbar,
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
-export default class Header extends React.Component<unknown, unknown> {
-    render(): JSX.Element {
-        return (
-            <header className="header">
-                <div className="header__content">
-                    <div className="header__content__main">
-                        <a href="/" className="header__link">
-                            GastonGit
-                        </a>
-                        <a
-                            href="https://github.com/GastonGit/"
-                            className="header__item"
+const pages = [{ name: 'GitHub', href: 'https://github.com/GastonGit' }];
+
+export default function Header(): JSX.Element {
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+        null,
+    );
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    return (
+        <AppBar
+            position="static"
+            style={{ background: 'transparent', boxShadow: 'none' }}
+        >
+            <Container maxWidth="md">
+                <Toolbar disableGutters>
+                    <Link
+                        href="/"
+                        variant="h6"
+                        underline="none"
+                        color="white"
+                        sx={{
+                            display: { md: 'flex' },
+                        }}
+                    >
+                        GastonGit
+                    </Link>
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: 'none', md: 'flex' },
+                            justifyContent: 'flex-end',
+                        }}
+                    >
+                        {pages.map((page) => (
+                            <Button
+                                key={page.name}
+                                onClick={handleCloseNavMenu}
+                                href={page.href}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                {page.name}
+                            </Button>
+                        ))}
+                    </Box>
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: 'flex', md: 'none' },
+                            justifyContent: 'flex-end',
+                        }}
+                    >
+                        <IconButton
+                            size="large"
+                            aria-label="url list"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
                         >
-                            <img src={githubLogo} alt="Github logo" />
-                        </a>
-                    </div>
-                </div>
-            </header>
-        );
-    }
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem
+                                    key={page.name}
+                                    onClick={handleCloseNavMenu}
+                                >
+                                    <Link
+                                        textAlign="center"
+                                        color="inherit"
+                                        href={page.href}
+                                        underline="none"
+                                    >
+                                        {page.name}
+                                    </Link>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 }
