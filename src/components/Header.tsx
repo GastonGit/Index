2e69427cc4ab404996c7e6701ab473f875930a77
GastonGit <73943682+GastonGit@users.noreply.github.com>
2022-01-26 13:19:56 +0100

    add fade in to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,10 +11,13 @@ import {
     Toolbar,
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import FadeIn from 'react-fade-in';
 
 const pages = [{ name: 'GitHub', href: 'https://github.com/GastonGit' }];
 
 export default function Header(): JSX.Element {
+    const fadeInDelay = 500;
+
     const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
         null,
     );
@@ -28,95 +31,101 @@ export default function Header(): JSX.Element {
     };
 
     return (
-        <AppBar
-            position="static"
-            style={{ background: 'transparent', boxShadow: 'none' }}
-        >
-            <Container maxWidth="md">
-                <Toolbar disableGutters>
-                    <Link
-                        href="/"
-                        variant="h6"
-                        underline="none"
-                        color="white"
-                        sx={{
-                            display: { md: 'flex' },
-                        }}
-                    >
-                        GastonGit
-                    </Link>
-                    <Box
-                        sx={{
-                            flexGrow: 1,
-                            display: { xs: 'none', md: 'flex' },
-                            justifyContent: 'flex-end',
-                        }}
-                    >
-                        {pages.map((page) => (
-                            <Button
-                                key={page.name}
-                                onClick={handleCloseNavMenu}
-                                href={page.href}
-                                sx={{ my: 2, color: 'white', display: 'block' }}
-                            >
-                                {page.name}
-                            </Button>
-                        ))}
-                    </Box>
-                    <Box
-                        sx={{
-                            flexGrow: 1,
-                            display: { xs: 'flex', md: 'none' },
-                            justifyContent: 'flex-end',
-                        }}
-                    >
-                        <IconButton
-                            size="large"
-                            aria-label="url list"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'left',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'left',
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
+        <FadeIn delay={fadeInDelay}>
+            <AppBar
+                position="static"
+                style={{ background: 'transparent', boxShadow: 'none' }}
+            >
+                <Container maxWidth="md">
+                    <Toolbar disableGutters>
+                        <Link
+                            href="/"
+                            variant="h6"
+                            underline="none"
+                            color="white"
                             sx={{
-                                display: { xs: 'block', md: 'none' },
+                                display: { md: 'flex' },
+                            }}
+                        >
+                            GastonGit
+                        </Link>
+                        <Box
+                            sx={{
+                                flexGrow: 1,
+                                display: { xs: 'none', md: 'flex' },
+                                justifyContent: 'flex-end',
                             }}
                         >
                             {pages.map((page) => (
-                                <MenuItem
+                                <Button
                                     key={page.name}
                                     onClick={handleCloseNavMenu}
+                                    href={page.href}
+                                    sx={{
+                                        my: 2,
+                                        color: 'white',
+                                        display: 'block',
+                                    }}
                                 >
-                                    <Link
-                                        textAlign="center"
-                                        color="inherit"
-                                        href={page.href}
-                                        underline="none"
-                                    >
-                                        {page.name}
-                                    </Link>
-                                </MenuItem>
+                                    {page.name}
+                                </Button>
                             ))}
-                        </Menu>
-                    </Box>
-                </Toolbar>
-            </Container>
-        </AppBar>
+                        </Box>
+                        <Box
+                            sx={{
+                                flexGrow: 1,
+                                display: { xs: 'flex', md: 'none' },
+                                justifyContent: 'flex-end',
+                            }}
+                        >
+                            <IconButton
+                                size="large"
+                                aria-label="url list"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                                color="inherit"
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left',
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    display: { xs: 'block', md: 'none' },
+                                }}
+                            >
+                                {pages.map((page) => (
+                                    <MenuItem
+                                        key={page.name}
+                                        onClick={handleCloseNavMenu}
+                                    >
+                                        <Link
+                                            textAlign="center"
+                                            color="inherit"
+                                            href={page.href}
+                                            underline="none"
+                                        >
+                                            {page.name}
+                                        </Link>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </FadeIn>
     );
 }
