a40d961579242e36a425df400b9cf0a5f4e14596
GastonGit <73943682+GastonGit@users.noreply.github.com>
2022-01-26 14:25:03 +0100

    convert App.tsx to a functional component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,46 +4,41 @@ import '../styles/App.css';
 import Grid from '@mui/material/Grid';
 import FadeIn from 'react-fade-in';
 
-export default class App extends React.Component<unknown, unknown> {
-    private welcomeFadeInDelay = 60;
-    private descriptionFadeInDelay = 250;
-    private projectsFadeInDelay = 500;
+export default function App(): JSX.Element {
+    const welcomeFadeInDelay = 60;
+    const descriptionFadeInDelay = 250;
+    const projectsFadeInDelay = 500;
 
-    render(): JSX.Element {
-        return (
-            <div className="app">
-                <Grid
-                    container
-                    className="app__content"
-                    rowSpacing={{ xs: 0, md: 3 }}
-                >
-                    <Grid item xs={12} md={12}>
-                        <FadeIn delay={this.welcomeFadeInDelay}>
-                            <p className="welcome__title">
-                                Welcome to GastonGit!
-                            </p>
-                            ;
-                        </FadeIn>
-                    </Grid>
-                    <Grid item xs={12} md={4}>
-                        <FadeIn delay={this.descriptionFadeInDelay}>
-                            <p className="description__text">
-                                This website showcases my most current projects.
-                                Feel free to check out the releases and live
-                                examples.
-                            </p>
-                        </FadeIn>
-                    </Grid>
-                    <Grid item className="flex--center" xs={12} md={8}>
-                        <FadeIn
-                            className="projects__fadein"
-                            delay={this.projectsFadeInDelay}
-                        >
-                            <Projects />
-                        </FadeIn>
-                    </Grid>
+    return (
+        <div className="app">
+            <Grid
+                container
+                className="app__content"
+                rowSpacing={{ xs: 0, md: 3 }}
+            >
+                <Grid item xs={12} md={12}>
+                    <FadeIn delay={welcomeFadeInDelay}>
+                        <p className="welcome__title">Welcome to GastonGit!</p>;
+                    </FadeIn>
                 </Grid>
-            </div>
-        );
-    }
+                <Grid item xs={12} md={4}>
+                    <FadeIn delay={descriptionFadeInDelay}>
+                        <p className="description__text">
+                            This website showcases my most current projects.
+                            Feel free to check out the releases and live
+                            examples.
+                        </p>
+                    </FadeIn>
+                </Grid>
+                <Grid item className="flex--center" xs={12} md={8}>
+                    <FadeIn
+                        className="projects__fadein"
+                        delay={projectsFadeInDelay}
+                    >
+                        <Projects />
+                    </FadeIn>
+                </Grid>
+            </Grid>
+        </div>
+    );
 }
