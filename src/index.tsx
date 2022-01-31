8011df58a2cbbb7aab2a84e1b606153f86fdc984
GastonGit <73943682+GastonGit@users.noreply.github.com>
2022-01-31 13:41:32 +0100

    remove fade from main page

--- src/index.tsx
+++ src/index.tsx
@@ -4,22 +4,12 @@ import App from './components/App';
 import Header from './components/Header';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-import { Fade } from '@mui/material';
-
-const mainFadeinDelay = 500;
-
 ReactDOM.render(
     <React.StrictMode>
-        <Fade
-            in={true}
-            timeout={mainFadeinDelay}
-            easing={'cubic-bezier(0.25, 0.1, 0.25, 0.1)'}
-        >
-            <div id="main">
-                <Header />
-                <App />
-            </div>
-        </Fade>
+        <div id="main">
+            <Header />
+            <App />
+        </div>
     </React.StrictMode>,
     document.getElementById('root'),
 );
