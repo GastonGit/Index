bc5673582c87ecf0e2a8561a60b17dabafee2e77
GastonGit <73943682+GastonGit@users.noreply.github.com>
2022-02-01 13:35:21 +0100

    add fadein for projects and refactor fade element locations

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import '../styles/App.css';
-import { Fade, Grid } from '@mui/material';
+import { Grid } from '@mui/material';
 
 export default function App(): JSX.Element {
-    const welcomeFadeInDelay = 500;
-    const descriptionFadeInDelay = 1500;
-
     return (
         <div className="app">
             <Grid
@@ -14,26 +11,13 @@ export default function App(): JSX.Element {
                 rowSpacing={{ xs: 0, md: 3 }}
             >
                 <Grid item xs={12} md={12}>
-                    <Fade
-                        in={true}
-                        timeout={welcomeFadeInDelay}
-                        easing={'cubic-bezier(0.25, 0.1, 0.25, 0.1)'}
-                    >
-                        <p className="welcome__title">Welcome to GastonGit!</p>
-                    </Fade>
+                    <p className="welcome__title">Welcome to GastonGit!</p>
                 </Grid>
                 <Grid item xs={12} md={12}>
-                    <Fade
-                        in={true}
-                        timeout={descriptionFadeInDelay}
-                        easing={'cubic-bezier(0.25, 0.1, 0.25, 0.1)'}
-                    >
-                        <p className="description__text">
-                            This website showcases my most current projects.
-                            Feel free to check out the releases and live
-                            examples.
-                        </p>
-                    </Fade>
+                    <p className="description__text">
+                        This website showcases my most current projects. Feel
+                        free to check out the releases and live examples.
+                    </p>
                 </Grid>
             </Grid>
         </div>
--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,31 @@ import Header from './components/Header';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import Projects from './components/Projects';
+import { Fade } from '@mui/material';
+
+const fadeInDelay = 1000;
+
 ReactDOM.render(
     <React.StrictMode>
-        <div id="main">
-            <Header />
-            <App />
-        </div>
-        <div id="projects-section">
-            <Projects />
-        </div>
+        <Fade
+            in={true}
+            timeout={fadeInDelay}
+            easing={'cubic-bezier(0.25, 0.1, 0.25, 0.1)'}
+        >
+            <div id="main">
+                <Header />
+                <App />
+            </div>
+        </Fade>
+        <Fade
+            in={true}
+            timeout={fadeInDelay}
+            easing={'cubic-bezier(0.25, 0.1, 0.25, 0.1)'}
+        >
+            <div id="projects-section">
+                <Projects />
+            </div>
+        </Fade>
     </React.StrictMode>,
     document.getElementById('root'),
 );
